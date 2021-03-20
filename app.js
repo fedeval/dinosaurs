4b8aef1b3e5d966b79883de0e28c74668cce25e9
fedeval <federico.valenza5@gmail.com>
2021-03-20 12:56:06 +0100

    feat: create dinosaur constructor funciton

--- app.js
+++ app.js
@@ -1,32 +1,40 @@
 
-    // Create Dino Constructor
+// Create Dino Constructor
+function Dinosaur(properties) {
+    this.species = properties.species;
+    this.weight = properties.weight;
+    this.height = properties.height;
+    this.diet = properties.diet;
+    this.where = properties.where;
+    this.when = properties.when;
+    this.fact = properties.fact;
+}
+
+// Create Dino Objects
 
 
-    // Create Dino Objects
+// Create Human Object
+
+// Use IIFE to get human data from form
 
 
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Remove form from screen
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+    // Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
